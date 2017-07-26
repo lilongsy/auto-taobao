361752efe3c4096a96648709a959c9cff09329d8
小龙 <lilongsy@gmail.com>
2017-07-26 09:48:39 +0800

    sort directory

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from config import Config
-from ProxyIp import ProxyIp
+from lib.ProxyIp import ProxyIp
 import time
 import os
 
